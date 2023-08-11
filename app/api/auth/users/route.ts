ebaa48f7e1b100fc18537eec8eacecfbdbe2ed48
Torinit-Prathamesh <104342622+Torinit-Prathamesh@users.noreply.github.com>
2023-08-11 15:28:15 +0530

    api auth user

--- app/api/auth/users/route.ts
+++ app/api/auth/users/route.ts
@@ -17,24 +17,28 @@ interface NewUserResponse {
 type NewResponse = NextResponse<{ user?: NewUserResponse; error?: string }>;
 
 export const POST = async (req: Request): Promise<NewResponse> => {
-  const body = (await req.json()) as NewUserRequest;
+  try {
+    const { email } = await req.json() as NewUserRequest;
 
-  await startDb().catch((error) => console.log(error));
+    await startDb(); 
 
-  const oldUser = await UserModal.findOne({ email: body.email });
-  if (oldUser)
-    return NextResponse.json(
-      { error: "email is already in use!" },
-      { status: 422 }
-    );
+    const existingUser = await UserModal.findOne({ email });
 
-  const user = await UserModal.create({ ...body });
+    if (existingUser) {
+      return NextResponse.json({ error: "Email is already in use!" }, { status: 422 });
+    }
 
-  return NextResponse.json({
-    user: {
-      id: user._id.toString(),
-      email: user.email,
-      name: user.name,
-    },
-  });
+    const newUser = await UserModal.create({ ...req.body });
+
+    const response: NewUserResponse = {
+      id: newUser._id.toString(),
+      email: newUser.email,
+      name: newUser.name,
+    };
+
+    return NextResponse.json({ user: response });
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+  }
 };
