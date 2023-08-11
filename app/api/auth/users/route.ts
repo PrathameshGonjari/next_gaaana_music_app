be9071cc6ae0f1ce4345e77defbb6f9dd241a579
Torinit-Prathamesh <104342622+Torinit-Prathamesh@users.noreply.github.com>
2023-08-11 15:34:11 +0530

    user auth

--- app/api/auth/users/route.ts
+++ app/api/auth/users/route.ts
@@ -25,7 +25,10 @@ export const POST = async (req: Request): Promise<NewResponse> => {
     const existingUser = await UserModal.findOne({ email });
 
     if (existingUser) {
-      return NextResponse.json({ error: "Email is already in use!" }, { status: 422 });
+      return new NextResponse(
+        JSON.stringify({ error: "Email is already in use!" }),
+        { status: 422, headers: { "Content-Type": "application/json" } }
+      );
     }
 
     const newUser = await UserModal.create({ ...req.body });
@@ -36,9 +39,15 @@ export const POST = async (req: Request): Promise<NewResponse> => {
       name: newUser.name,
     };
 
-    return NextResponse.json({ user: response });
+    return new NextResponse(
+      JSON.stringify({ user: response }),
+      { headers: { "Content-Type": "application/json" } }
+    );
   } catch (error) {
     console.error("Error:", error);
-    return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+    return new NextResponse(
+      JSON.stringify({ error: "An error occurred" }),
+      { status: 500, headers: { "Content-Type": "application/json" } }
+    );
   }
 };
