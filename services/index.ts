23d7075dde55fa6af4ea62b07e4b47a24170f3ea
Torinit-Prathamesh <104342622+Torinit-Prathamesh@users.noreply.github.com>
2023-09-09 15:05:35 +0530

    services file formatted

--- services/index.ts
+++ services/index.ts
@@ -2,43 +2,41 @@ import axios from "axios";
 
 class Services {
   get = async (url: string, params?: never) => {
-    return new Promise((resolve, reject) => {
+    return await new Promise((resolve, reject) => {
       try {
         axios
           .get(`${process.env.NEXT_PUBLIC_BASE_URL as string}${url}`, params)
           .then((res) => {
-            return resolve({ data: res });
+            resolve({ data: res });
           })
-          .catch((err) => {
-            return reject(console.log("error has accoured", err));
+          .catch(() => {
+            reject(new Error("error has accoured"));
           });
       } catch (error) {
-        return reject(console.log("error has accoured", error));
+        reject(new Error("error has accoured"));
       }
     });
   };
 
-  post = async (
-    url: string,
-    params?: unknown,
-  ) => {
-    return new Promise((resolve, reject) => {
+  post = async (url: string, params?: unknown) => {
+    return await new Promise((resolve, reject) => {
       try {
         axios
           .post(`${process.env.NEXT_PUBLIC_BASE_URL as string}${url}`, {
             headers: {
+              // eslint-disable-next-line @typescript-eslint/naming-convention
               "Content-Type": "application/json",
             },
             params,
           })
           .then((res) => {
-            return resolve({ data: res });
+            resolve({ data: res });
           })
-          .catch((err) => {
-            return reject(console.log("error has accoured", err));
+          .catch(() => {
+            reject(new Error("error has accoured"));
           });
       } catch (error) {
-        return reject(console.log("error has accoured", error));
+        reject(new Error("error has accoured"));
       }
     });
   };
